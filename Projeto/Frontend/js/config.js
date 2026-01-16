0b35afbdf26c1fc94e469de90b5e1556a48c6610
Alexandre Freitas <148783133+Freitas29522@users.noreply.github.com>
2026-01-16 15:51:32 +0000

    V.3.2.6

--- Projeto/Frontend/js/config.js
+++ Projeto/Frontend/js/config.js
@@ -2,7 +2,9 @@
   const LOCAL_API = "https://localhost:44311";
   const PROD_API  = "http://192.168.1.54:81";
 
-  const isLocal = location.hostname === "localhost" || location.hostname === "127.0.0.1";
+  const isLocal = 
+    location.hostname === "localhost" || 
+    location.hostname === "127.0.0.1";
 
   window.APP_CONFIG = {
     API_BASE_URL: isLocal ? LOCAL_API : PROD_API,
